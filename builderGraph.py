0484bbc59a52a9a6dea638492383d5f95fe07ed1
biplavpoudel <biplavpoudel764@gmail.com>
2025-05-22 21:44:11 +0545

    added vector store retriever as a node; unsure if it all fits

--- builderGraph.py
+++ builderGraph.py
@@ -3,12 +3,13 @@ from typing import TypedDict, List, Dict, Any, Optional, Annotated
 from langgraph.graph import StateGraph, START, END
 from langchain_core.messages import HumanMessage, AIMessage, AnyMessage, SystemMessage
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
-from langgraph.graph.message import add_messages
 from tools import add, modulo, divide, multiply, subtract, arxiv_search, web_search, wiki_search
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import Distance, VectorParams
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.documents import Document
 
 from langchain_ollama import ChatOllama
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -23,14 +24,15 @@ tools = [add, subtract, multiply, divide, modulo, arxiv_search, web_search, wiki
 
 
 class State(TypedDict):
-    messages: List[AnyMessage, add_messages]
+    messages: List[AnyMessage]
 
 
 def build_graph(llm_provider: str = 'gemma'):
     # Adding LLM/VLM Model
     if llm_provider == "huggingface":
         llm = HuggingFaceEndpoint(model='mistralai/Mistral-7B-Instruct-v0.3',
-                                  # huggingfacehub_api_token=os.getenv('HUGGINGFACE_API')
+                                  # huggingfacehub_api_token=os.getenv('HUGGINGFACE_API'),
+                                  verbose=True,
         )
     elif llm_provider == "gemma":
         llm = ChatGoogleGenerativeAI(model="gemini/gemini-2.0-flash-lite-001")
@@ -40,7 +42,7 @@ def build_graph(llm_provider: str = 'gemma'):
             temperature=0)
     else:
         raise ValueError(f"Unknown LLM provider: {llm_provider}")
-    llm.bind_tools(tools)
+    llm_with_tools = llm.bind_tools(tools)
 
     # Adding Embedding Model
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
@@ -58,15 +60,37 @@ def build_graph(llm_provider: str = 'gemma'):
         embedding=embeddings,
     )
 
+    graph_builder = StateGraph()
+    retriever = vector_store.as_retriever()
+
+    # Creating Node
     def assistant(state: State):
-        return llm.invoke(state["messages"])
+        return {
+            "message": [llm_with_tools.invoke(state["messages"])]
+        }
+    def retrieve_documents(state: State):
+        latest_user_msg = [msg.content for msg in state["messages"] if isinstance(msg,HumanMessage)][-1]
+        retrieved_docs = retriever.retrieve(latest_user_msg)
+        return {
+            "message": state["messages"] + [SystemMessage(content="\n\n".join([doc.content for doc in retrieved_docs ]))]
+        }
 
+
+    # Add Nodes to Graph
     graph_builder.add_node("assistant", assistant)
-    graph_builder.add_edge(START, "assistant")
+    graph_builder.add_node("tools", ToolNode(tools))
+    graph_builder.add_node("document_retriever", retrieve_documents)
+
+    # Add Edges to Graph
+    graph_builder.add_edge(START, "document_retriever")
+    graph_builder.add_edge( "document_retriever", "assistant")
+    graph_builder.add_conditional_edges(
+        "assistant",
+        # If the latest message requires a tool, route to tools
+        # Otherwise, provide a direct response
+        tools_condition,
+    )
+    graph_builder.add_edge("tools", "assistant")
+
+    # Build Graph
     graph = graph_builder.compile()
-
-
-
-
-
-graph_builder = StateGraph()
