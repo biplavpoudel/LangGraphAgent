f1c646fa9b682c53bb78000df6983f69626b2f72
biplavpoudel <biplavpoudel764@gmail.com>
2025-05-22 14:28:02 +0545

    ddgs api is now working

--- tools.py
+++ tools.py
@@ -1,13 +1,8 @@
-import os
-import json
-
-from PIL.features import features
 from duckduckgo_search.exceptions import DuckDuckGoSearchException
 from langchain_community.docstore import Wikipedia
-from langchain_community.tools import DuckDuckGoSearchResults
 from langchain_core.tools import tool
-from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
 from langchain_tavily import TavilySearch
+from duckduckgo_search import DDGS
 
 # from langchain_community.utilities import SearxSearchWrapper
 from langchain_community.document_loaders import WikipediaLoader
@@ -80,30 +75,28 @@ def web_search(query: str) -> str:
         query: String to search for
     """
     try:
-        wrapper = DuckDuckGoSearchAPIWrapper(region="en-us", max_results=2)
-        search = DuckDuckGoSearchResults(api_wrapper=wrapper, output_format="json")
-        result = search.invoke(query)
-        # formatted_result = f"\n{'-' * 10}\n".join([
-        #     f"<Document Source = '{page.metadata['source']}', Page = '{page.metadata['page']}'>\n {page.content}"
-        #     for page in result])
-        return result
+        results = DDGS().text(keywords=query, max_results=2)
+        formatted_result = f"\n\n{'-' * 50}\n\n".join([
+            f"<Document Source = '{page['href']}', Title = '{page['title']}'>\n {page['body']}"
+            for page in results])
+        return formatted_result
     except DuckDuckGoSearchException as e:
         print(f"Exception occur with DuckDuckGo! {str(e)}. \n Trying another search engine ")
+
+
+    try:
+        search_tool = TavilySearch(
+            max_results=2,
+            topic="general")
+        result = search_tool.invoke(f'"query":"{query}"')["results"]
+        formatted_result = f"\n\n{'-' * 50}\n\n".join([
+            f"<Document Source = '{page['url']}', Title = '{page['title']}'>\n {page['content']}"
+            for page in result])
+        return formatted_result
+
+    except Exception as  e:
+        print(f"Exception occur with Tavily! {str(e)}. \n Returning empty json")
         return ""
-    #
-    # try:
-    #     search_tool = TavilySearch(
-    #         max_results=2,
-    #         topic="general")
-    #     result = search_tool.invoke(f'"query":"{query}"')
-    #     # formatted_result = f"\n{'-' * 10}\n".join([
-    #     #     f"<Document Source = '{page.metadata['source']}', Page = '{page.metadata['page']}'>\n {page.content}"
-    #     #     for page in result])
-    #     return result
-    #
-    # except Exception as  e:
-    #     print(f"Exception occur with Tavily! {str(e)}. \n Returning empty json")
-    #     return ""
 
 
 @tool("wiki_search", parse_docstring=False)
@@ -127,4 +120,3 @@ if __name__ == "__main__":
     keyword = "Martin Luther King"
     # print(wiki_search.invoke(keyword))
     print(web_search.invoke(keyword))
-    # print(divide.invoke({"a": 2, "b": 4}))
