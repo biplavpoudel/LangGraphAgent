685afbe19a1b0ee04537be12ecb3ab7907d16b80
biplavpoudel <biplavpoudel764@gmail.com>
2025-05-23 11:52:19 +0545

    added sys_prompt to assistant node; unsure if I should use embeddings

--- builderGraph.py
+++ builderGraph.py
@@ -14,26 +14,17 @@ from langchain_core.documents import Document
 from langchain_ollama import ChatOllama
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-# Let's add system prompt from system_prompt.txt
-with open("system_prompt.txt") as f:
-    sys_message = f.read()
-system_prompt = SystemMessage.from_text(sys_message)
-
-# List of Tools that are later bound to the LLM
-tools = [add, subtract, multiply, divide, modulo, arxiv_search, web_search, wiki_search]
-
-
 class State(TypedDict):
     messages: List[AnyMessage]
-
 
 def build_graph(llm_provider: str = 'gemma'):
     # Adding LLM/VLM Model
     if llm_provider == "huggingface":
-        llm = HuggingFaceEndpoint(model='mistralai/Mistral-7B-Instruct-v0.3',
+        llm = HuggingFaceEndpoint(repo_id='mistralai/Mistral-7B-Instruct-v0.3',
+                                  model='mistralai/Mistral-7B-Instruct-v0.3',
                                   # huggingfacehub_api_token=os.getenv('HUGGINGFACE_API'),
                                   verbose=True,
-        )
+                                  )
     elif llm_provider == "gemma":
         llm = ChatGoogleGenerativeAI(model="gemini/gemini-2.0-flash-lite-001")
     elif llm_provider == "ollama":
@@ -42,6 +33,15 @@ def build_graph(llm_provider: str = 'gemma'):
             temperature=0)
     else:
         raise ValueError(f"Unknown LLM provider: {llm_provider}")
+
+    # Let's add system prompt from system_prompt.txt
+    with open("system_prompt.txt") as f:
+        sys_message = f.read()
+    system_prompt = SystemMessage.from_text(sys_message)
+
+    # List of Tools that are bound to the LLM
+    tools = [add, subtract, multiply, divide, modulo, arxiv_search, web_search, wiki_search]
+
     llm_with_tools = llm.bind_tools(tools)
 
     # Adding Embedding Model
@@ -49,7 +49,8 @@ def build_graph(llm_provider: str = 'gemma'):
 
     # Adding Vector Store
     # vector_store = InMemoryVectorStore(embeddings)
-    client = QdrantClient(url="https://e75c4d7b-d8c8-4f7b-9e54-892bc1583cb9.europe-west3-0.gcp.cloud.qdrant.io:6333", api_key=os.environ["QDRANT_API_KEY"])
+    client = QdrantClient(url="https://e75c4d7b-d8c8-4f7b-9e54-892bc1583cb9.europe-west3-0.gcp.cloud.qdrant.io:6333",
+                          api_key=os.environ["QDRANT_API_KEY"])
     client.create_collection(
         collection_name="langchain_assistant",
         vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
@@ -66,15 +67,15 @@ def build_graph(llm_provider: str = 'gemma'):
     # Creating Node
     def assistant(state: State):
         return {
-            "message": [llm_with_tools.invoke(state["messages"])]
-        }
-    def retrieve_documents(state: State):
-        latest_user_msg = [msg.content for msg in state["messages"] if isinstance(msg,HumanMessage)][-1]
-        retrieved_docs = retriever.retrieve(latest_user_msg)
-        return {
-            "message": state["messages"] + [SystemMessage(content="\n\n".join([doc.content for doc in retrieved_docs ]))]
+            "message": [llm_with_tools.invoke(state["messages"] + [system_prompt])],
         }
 
+    def retrieve_documents(state: State):
+        latest_user_msg = [msg.content for msg in state["messages"] if isinstance(msg, HumanMessage)][-1]
+        retrieved_docs = retriever.retrieve(latest_user_msg)
+        return {
+            "message": state["messages"] + [SystemMessage(content="\n\n".join([doc.content for doc in retrieved_docs]))]
+        }
 
     # Add Nodes to Graph
     graph_builder.add_node("assistant", assistant)
@@ -83,7 +84,7 @@ def build_graph(llm_provider: str = 'gemma'):
 
     # Add Edges to Graph
     graph_builder.add_edge(START, "document_retriever")
-    graph_builder.add_edge( "document_retriever", "assistant")
+    graph_builder.add_edge("document_retriever", "assistant")
     graph_builder.add_conditional_edges(
         "assistant",
         # If the latest message requires a tool, route to tools
